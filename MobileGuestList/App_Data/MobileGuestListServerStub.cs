930df2c1169db21d2a710f70de189b39504bfc20
AlexOg37 <alexog37@gmail.com>
2015-08-04 10:20:22 +0200

    solution commit

---
+++ MobileGuestList/App_Data/MobileGuestListServerStub.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Globalization;
+using MobileGuestList.Models;
+
+namespace MobileGuestList.App_Data
+{
+	public class MobileGuestListServerStub : IMobileGuestListServer
+	{
+		public string ValidateUser(string strUsername, string strPassword)
+		{
+			return "4323243jlkj4l32k4j324lk32j4lk243jlk32j4";
+		}
+		public ProfileModel GetProfile(string strQuerystring)
+		{
+			ProfileModel profile = new ProfileModel();
+
+			profile.Username = "Unknown";
+			profile.UserID = int.MaxValue;
+			profile.SQLDB = "SQLDB";
+			profile.CultureFormat = DateTimeFormatInfo.CurrentInfo;
+			profile.TimeZone = TimeZone.CurrentTimeZone;
+			profile.StationID = int.MaxValue;
+			profile.AccessToGuestList = true;
+
+			return profile;
+		}
+		public IEnumerable<Contest> GetContests()
+		{
+			List<Contest> list = new List<Contest>();
+
+			list.Add(new Contest { Id = 1, Name = "Contest1" });
+			list.Add(new Contest { Id = 2, Name = "Contest2" });
+			list.Add(new Contest { Id = 3, Name = "Contest3" });
+
+			return list;
+		}
+		public IEnumerable<Guest> GetGuests(Contest contest)
+		{
+			List<Guest> list = new List<Guest>();
+
+			list.Add(new Guest { Id = 1, Name = "Oleg", Birthdate = new DateTime(1970, 7, 7), HomeCity="Kharkov", BusinessCity="Budva", HomePhone="717-34-75", MobilePhone="050-345-75-91", BusinessPhone="717-59-34", FulfillmentDate = DateTime.Now });
+			list.Add(new Guest { Id = 2, Name = "Alex", Birthdate = new DateTime(1980, 8, 8), HomeCity = "Kharkov", BusinessCity = "Budva", HomePhone = "717-75-83", MobilePhone = "095-572-97-77", BusinessPhone = "717-56-21" });
+			list.Add(new Guest { Id = 3, Name = "Vlad", Birthdate = new DateTime(1990, 9, 9), HomeCity = "Kharkov", BusinessCity = "Budva", HomePhone = "717-67-34", MobilePhone = "095-789-34-21", BusinessPhone = "717-45-90" });
+
+			return list;
+		}
+		public void MarkDistributed(Contest contest, DateTime date)
+		{
+		}
+		public void Attend(int iGuestId)
+		{
+		}
+	}
+}
---
+++ MobileGuestList/Controllers/AccountController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.Profile;
+using System.Globalization;
+using MobileGuestList.App_Data;
+using MobileGuestList.Models;
+
+namespace MobileGuestList.Controllers
+{
+
+	[Authorize]
+	public class AccountController : Controller
+	{
+		[AllowAnonymous]
+		public ActionResult Login()
+		{
+			return View();
+		}
+
+		[AllowAnonymous]
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Login(LoginModel model)
+		{
+			string returnUrl = "";
+			if (ModelState.IsValid)
+			{
+				if (Membership.ValidateUser(model.Username, model.Password))
+				{
+					FormsAuthentication.SetAuthCookie(model.Station, model.RememberStation);
+
+					string strLoginCode = "";
+					HttpSessionStateBase session = HttpContext.Session;
+					session["LoginCode"] = strLoginCode;
+
+					if (Url.IsLocalUrl(returnUrl))
+					{
+						return Redirect(returnUrl);
+					}
+					else
+					{
+						return RedirectToAction("Selection", "Contest");
+					}
+				}
+				else
+				{
+					ModelState.AddModelError("", "The user name or password provided is incorrect.");
+				}
+			}
+
+			// If we got this far, something failed, redisplay form
+			return View(model);
+		}
+
+		public ActionResult LogOff()
+		{
+			FormsAuthentication.SignOut();
+
+			return RedirectToAction("Login");
+		}
+
+		#region Status Codes
+		private static string ErrorCodeToString(MembershipCreateStatus createStatus)
+		{
+			// See http://go.microsoft.com/fwlink/?LinkID=177550 for
+			// a full list of status codes.
+			switch (createStatus)
+			{
+				case MembershipCreateStatus.DuplicateUserName:
+					return "User name already exists. Please enter a different user name.";
+
+				case MembershipCreateStatus.DuplicateEmail:
+					return "A user name for that e-mail address already exists. Please enter a different e-mail address.";
+
+				case MembershipCreateStatus.InvalidPassword:
+					return "The password provided is invalid. Please enter a valid password value.";
+
+				case MembershipCreateStatus.InvalidEmail:
+					return "The e-mail address provided is invalid. Please check the value and try again.";
+
+				case MembershipCreateStatus.InvalidAnswer:
+					return "The password retrieval answer provided is invalid. Please check the value and try again.";
+
+				case MembershipCreateStatus.InvalidQuestion:
+					return "The password retrieval question provided is invalid. Please check the value and try again.";
+
+				case MembershipCreateStatus.InvalidUserName:
+					return "The user name provided is invalid. Please check the value and try again.";
+
+				case MembershipCreateStatus.ProviderError:
+					return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+				case MembershipCreateStatus.UserRejected:
+					return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+				default:
+					return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+			}
+		}
+		#endregion
+	}
+}
