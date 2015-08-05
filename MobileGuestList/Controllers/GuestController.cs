e0277cbf3b5ec2e606b9b158cd20008ff33d69bb
AlexOg37 <alexog37@gmail.com>
2015-08-05 19:40:37 +0200

    Revert "StyleChanges"
    This reverts commit 31f7736b7d7b390b807a7af6311b43d1eb4cef89.

---
+++ MobileGuestList/Controllers/GuestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MobileGuestList.App_Data;
+using MobileGuestList.Models;
+
+namespace MobileGuestList.Controllers
+{
+	public class GuestController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Location = "Listener Database > Guest Lists";
+
+			HttpSessionStateBase session = HttpContext.Session;
+			Contest contest = (Contest)session["Contest"];
+			ViewBag.Contest = contest;
+
+			HttpApplicationStateBase application = HttpContext.Application;
+			IMobileGuestListServer server = (IMobileGuestListServer)application["IMobileGuestListServer"];
+			ViewBag.Guests = server.GetGuests(contest);
+
+			return View();
+		}
+
+	}
+}
