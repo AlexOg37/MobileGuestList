ea0c9dc1a4f91b5ce76a36f9494f9cfc77ac92e6
AlexOg37 <alexog37@gmail.com>
2015-08-07 13:20:06 +0200

    small fix

---
+++ MobileGuestList/App_Start/UnityConfig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using Unity.Mvc4;
+
+namespace MobileGuestList.App_Start
+{
+    public class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            //var container = new UnityContainer();
+            //RegisterTypes(container);
+
+            //DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+
+        private static void RegisterTypes(IUnityContainer container)
+        {
+
+            #region Integration
+
+           // container.RegisterType<IUserControllerProvider, UserControllerProvider>();
+
+            #endregion
+        }
+    }
+}
