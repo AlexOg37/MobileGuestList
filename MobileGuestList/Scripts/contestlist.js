4e1758d5317a2e1239cc453dbd41277e79bbb1ad
DmytroA <dand2mj@gmail.com>
2015-08-14 19:19:04 +0300

    InitSorting

--- MobileGuestList/Scripts/contestlist.js
+++ MobileGuestList/Scripts/contestlist.js
@@ -14,7 +14,34 @@ function OnCurrentContestChanged() {
     });
 }
 
+function InitSorting() {
+    defaultList = $("#contestSelect").html();
+    $('#myonoffswitch').change(function () {
+        var test = $("#contestSelect").val();
 
+        if ($(this).prop("checked")) {
+            var my_options = $("#contestSelect option");
+            var sortData = my_options.filter('[data-sort="yes"]');
+
+            sortData.sort(function (a, b) {
+
+                if (a.text > b.text) return 1;
+                else if (a.text < b.text) return -1;
+                else return 0
+            });
+            var noSortData = my_options.filter('[data-sort="no"]');
+            $("#contestSelect").empty().append(noSortData).append(sortData);
+
+            //$("#contestSelect [value='" + test + "']").attr("selected", "selected");
+            $("#contestSelect").val(test);
+        }
+        else {
+            $("#contestSelect").html(defaultList);
+            $("#contestSelect").val(test);
+
+        }
+    });
+}
 $(document).ready(function () {
     var defaultList = null;
     defaultList = $("#contestSelect").html();
