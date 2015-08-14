518491f2d92b4d9a8b7d3b5ad02c80fc67634013
AlexOg37 <alexog37@gmail.com>
2015-08-14 18:21:46 +0200

    change station script fix

--- MobileGuestList/Scripts/changeStation.js
+++ MobileGuestList/Scripts/changeStation.js
@@ -1,5 +1,8 @@
-﻿function changeStation(item) {
-    $.post('/Home/ChangeStation?stationId=' + $(item).attr('data-station-id'), function (result) {
+﻿/// <reference path="contestlist.js" />
+function changeStation() {
+    var item = this;
+    var actionUrl = $("#toggler").attr("data-action");
+    $.post(actionUrl + '?stationId=' + $(item).attr('data-station-id'), function (result) {
         $("#contestSelect").html("");
         $("#contestSelect").append("<option selected='selected' value='' data-sort='no'>Pick Your Contest</option>");
         $(result).each(function (index, item) {
@@ -13,3 +16,7 @@
     $('#station_call').text($(item).text());
 
 }
+
+$(document).ready(function () {
+    $("#toggler>div").click(changeStation);
+});
